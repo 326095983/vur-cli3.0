1894218d65a0afd81a8b5a73d0ecfc15306942de
326095983 <326095983@qq.com>
2019-10-20 21:59:59 +0800

    11

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+const Registe = () => import('../views/Registe/Registe')
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    redirect: "/registe"
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/registe",
+    name: "registe",
+    component: Registe
   }
 ];
 
---
+++ src/views/Registe/Registe.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="registe-content">
+    <h1>这是注册页面</h1>
+  </div>
+</template>
