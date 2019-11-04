e2d15bbec49619c326cf10d02aa2ec03be039947
xiaowu <326095983@qq.com>
2019-11-04 15:21:21 +0800

    11

--- src/http/request.js
+++ src/http/request.js
@@ -1,10 +1,3 @@
-import flyio from 'flyio';
-let server = 'http://121.40.240.199:8001/api'
-//判断是否是本地还是正式
-// process.env.NODE_ENV == 'production'?server = window.location.origin + '/ms':server = '/api'
-//flyio请求
-flyio.config.headers={'Content-Type': 'application/json','Accept': 'application/json'}
-
 // import { Loading, Message } from "element-ui";
 // let loading;
 // function startLoading() {
@@ -19,26 +12,37 @@ flyio.config.headers={'Content-Type': 'application/json','Accept': 'application/
 //   loading.close();
 // }
 
+import flyio from 'flyio'
+let server = 'http://121.40.240.199:8001/api'//正式
+// let server = 'http://192.168.0.102:8001/api'//本地
+//判断是否是本地还是正式
+// process.env.NODE_ENV == 'production'?server = window.location.origin + '/ms':server = '/api'
+//flyio请求
+
+// flyio.config.headers={}
 let httpRequest = async function(...option){
   let [method,httpUrl,body] = option
   try {
-    // startLoading();
     let requestData = {
       method:method,
+      headers:{
+        'token':sessionStorage.getItem('token'),
+        'Content-Type':'application/json',
+        "accept": "application/json",
+      },
       url: server + httpUrl,
       // parseJson:true,
       withCredentials:true,
       body:body,
     }
     let res = await flyio.request(requestData);
-    // endLoading();
     return res.data
   }catch(error){
-    // endLoading();
     console.log(error)
     return {code:1, message:error.message};
   }
 }
 export default {
-  httpRequest, server
+  httpRequest,server
 };
+
--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    redirect: "/register"
+    redirect: "/login"
   },
   {
     path: "*",
